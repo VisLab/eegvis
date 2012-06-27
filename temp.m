3954e5cbb7926ff7e3d8e0482545045cd6e2539f
Kay Robbins <krobbins@cs.utsa.edu>
2012-06-27 15:29:16 -0500

    Fixed documentation

--- temp.m
+++ temp.m
@@ -1,15 +1,25 @@
-load('EEGEmotiv.mat')
+load('EEGArtifact.mat');
+values.EEGArtifact = EEGArtifact;
+load('ArtifactLabels.mat');
+values.artifactEvents = artifactEvents;
+%%
+pS = viewTestClass.getDefaultPlotsWithEvents();
+assertEqual(length(pS), 10);
+testVD3 = viscore.blockedData(values.EEGArtifact.data, ...
+    'Artifact (Artifact events)', 'Events', values.artifactEvents, ...
+    'BlockSize', 1000, 'SampleRate', values.EEGArtifact.srate);
+bv3 = visviews.dualView('VisData', testVD3, 'Plots', pS');
+assertTrue(isvalid(bv3));
+
 
 %%
-[event, sTimes, tScale] = viscore.eventData.getEEGTimes(EEGEmotiv);
-
-
-%%
-types = results.predicted;
-startTimes = num2cell((results.t)');
-certainties =  num2cell((results.confidence)');
-%%
-events = struct('type', types, 'startTime', startTimes, 'certainty', certainties);
-
-%%
-save('ArtifactLabels.mat', 'artifactEvents');
+x = figure
+y = findall(x);
+ for k = 1:length(y)
+     p = get(y(k), 'Type');
+    fprintf('%d: %s ', k, get(y(k), 'Tag'));
+    if strcmp(p, 'uimenu')
+        fprintf('%s', get(y(k), 'Label'));
+    end
+    fprintf('\n');
+end
